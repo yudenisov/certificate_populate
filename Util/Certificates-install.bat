f6ab9a92b02be628ccb2045f7e5f9747ec15dcc7
Юрий А. Денисов <yudenisov@mail.ru>
2020-08-20 05:55:15 +0400

    Transfer

--- Util/Certificates-install.bat
+++
@@ -1,6 +0,0 @@
-rem Install New Internet Technologies Certificates
-if not exist Distrib\certificates-all.reg goto pass_Certificates
-echo "Install Certificates..."
-%SystemRoot%\system32\reg.exe import Distrib\certificates-all.reg
-:pass_Certificates
-
--- Util/pvk2pfxcmd.bat
+++
@@ -1,2 +0,0 @@
-"%~dp0Cert2Spc.exe" %1.cer %1.spc
-"%~dp0pvk2pfx.exe" -pvk %1.pvk -spc %1.spc -pfx %1.pfx -f
